6eb237573e4828d80ce4a5f652fa8fe05f42957c
Ruzbihan <ruzbihanmzm@gmail.com>
2024-05-25 12:57:25 +0530

    9. Fixing Bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,7 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50"
-        />
-      </div>
+    <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -30,7 +23,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-2">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright &copy; 2024 Ruzbihan
         </p>
